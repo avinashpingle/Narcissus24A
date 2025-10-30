e17d515df9252c046b047158cdb788fed3302601
avinashpingle <avinashpingle@gmail.com>
2025-10-30 20:56:37 +0530

    Updating project

--- src/test/java/com/skillio/FeatureRunner.java
+++ src/test/java/com/skillio/FeatureRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Features",
 glue = "com.skillio",
-tags = "@Smoke",
 dryRun=false)
 public class FeatureRunner extends AbstractTestNGCucumberTests{
 
